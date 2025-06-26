26a56fabe623a1a734053229e1693d50bce51943
John Yeo <johnyeocx@gmail.com>
2025-06-26 20:34:46 +0100

    added sync-registries

--- package/src/libraries/react/index.ts
+++ package/src/libraries/react/index.ts
@@ -5,3 +5,8 @@ export { useCustomer } from "./hooks/useCustomer";
 export { useAutumn } from "./hooks/useAutumn";
 export { usePricingTable } from "./hooks/usePricingTable";
 export { useEntity } from "./hooks/useEntity";
+
+
+// Auto-synced components
+export { default as AttachDialog } from "./components/attach-dialog/attach-dialog-synced";
+export { default as CheckDialog } from "./components/check-dialog/check-dialog-synced";
