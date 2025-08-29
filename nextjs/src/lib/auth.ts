1fb4cf2661a7b94e9b3d9f45b754c38ad5c8fdc0
amianthus <49116958+SirTenzin@users.noreply.github.com>
2025-08-29 19:39:50 +0100

    feat: 🎸 sqlite and autumn(identify())

--- nextjs/src/lib/auth.ts
+++ nextjs/src/lib/auth.ts
@@ -1,22 +1,37 @@
-import { betterAuth } from "better-auth";
-import { organization } from "better-auth/plugins";
+import { betterAuth, Session } from "better-auth";
+import { Organization, organization } from "better-auth/plugins";
 import { autumn } from "autumn-js/better-auth";
-
-import { Pool } from "pg";
+import Database from "bun:sqlite";
 
 export const auth = betterAuth({
-  database: new Pool({
-    connectionString: process.env.DATABASE_URL,
-  }),
-  trustedOrigins: ["http://localhost:3000", "http://localhost:3001"],
-  secret: process.env.BETTER_AUTH_SECRET,
-  emailAndPassword: {
-    enabled: true,
-  },
-  plugins: [organization(), autumn()],
+	database: new Database("database.sqlite"),
+	trustedOrigins: ["http://localhost:3000", "http://localhost:3001"],
+	secret: process.env.BETTER_AUTH_SECRET,
+	emailAndPassword: {
+		enabled: true,
+	},
+	plugins: [
+		organization(),
+		autumn({
+			identify: ({ session, organization }) => {
+				if (organization?.id) {
+					return {
+						customerId: organization.id ?? null,
+						customerData: {
+							email: organization.ownerEmail ?? null,
+							name: organization.name ?? null,
+						},
+					};
+				} else if (session?.user.id) {
+					return {
+						customerId: session.user.id ?? null,
+						customerData: {
+							email: session.user.email ?? null,
+							name: session.user.name ?? null,
+						},
+					};
+				} else return null;
+			},
+		}),
+	],
 });
-
-// {
-//   url: "http://localhost:8080/v1",
-//   secretKey: process.env.AUTUMN_SECRET_KEY,
-// }
