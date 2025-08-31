be2dfd552c7dae2e5f4ea5ad6ac87e6b2967bb33
Zetax <ernzerhofffinn@gmail.com>
2025-08-31 02:25:04 +0200

    feat: add setBalances method to update customer balances

--- package/src/sdk/features/featureMethods.ts
+++ package/src/sdk/features/featureMethods.ts
@@ -3,10 +3,16 @@ import { Autumn } from "../client";
 import { AutumnPromise } from "../response";
 import { staticWrapper } from "../utils";
 import { Feature } from "./featureTypes";
+import type {
+  UpdateBalancesParams,
+  UpdateBalancesResult,
+} from "../customers/cusTypes";
 
 export const featureMethods = (instance?: Autumn) => {
   return {
     list: () => staticWrapper(listFeatures, instance, {}),
+    setBalances: (customer_id: string, params: UpdateBalancesParams) =>
+      staticWrapper(setBalances, instance, { customer_id, params }),
   };
 };
 
@@ -33,4 +39,18 @@ export const listFeatures = async ({
     }
   }
   return instance.get(path);
+};
+
+export const setBalances = async ({
+  instance,
+  customer_id,
+  params,
+}: {
+  instance: Autumn;
+  customer_id: string;
+  params: UpdateBalancesParams;
+}): AutumnPromise<UpdateBalancesResult> => {
+  return instance.post(`/customers/${customer_id}/balances`, {
+    balances: Array.isArray(params) ? params : [params],
+  });
 };
