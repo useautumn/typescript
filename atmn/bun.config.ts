fc3fe8e230413e1303e4114946c53cb08cd11130
amianthus <49116958+SirTenzin@users.noreply.github.com>
2025-09-02 16:02:16 +0100

    feat: 🎸 bun building

--- atmn/bun.config.ts
+++ atmn/bun.config.ts
@@ -1,9 +1,18 @@
+async function getVersion(): Promise<string> {
+	const packageJson = await Bun.file("./package.json").json();
+	return packageJson.version;
+}
+
 async function build() {
+	console.log(`Building atmn v${await getVersion()}`);
 	await Bun.build({
 		entrypoints: ["./source/cli.ts", "./source/index.ts"],
 		outdir: "./dist",
 		format: "esm",
 		target: "node",
+		define: {
+			VERSION: `"${await getVersion()}"`,
+		}
 	});
 
 	// Generate TypeScript declaration files
