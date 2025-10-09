2e2141668c331d51579cf22ea9ecf36931bb1fee
John Yeo <johnyeocx@gmail.com>
2025-10-09 16:17:50 +0100

    fix: typegen

--- autumn-js/src/libraries/react/clientTypes/checkoutTypes.ts
+++ autumn-js/src/libraries/react/clientTypes/checkoutTypes.ts
@@ -14,20 +14,19 @@ export const CheckoutParamsOptionSchema = z.object({
 });
 
 export const CheckoutParamsSchema = z.object({
-  checkoutSessionParams: z.unknown().optional(),
-  customerData: CustomerDataSchema.describe("Customer data if using attach to auto create customer").optional(),
-  entityData: EntityDataSchema.describe("Entity data for creating an entity").optional(),
-  entityId: z.string().nullable().optional(),
-  forceCheckout: z.boolean().optional(),
-  freeTrial: z.boolean().optional(),
+  checkoutSessionParams: z.unknown().describe("Additional parameters to pass onto Stripe when creating the checkout session").optional(),
+  customerData: CustomerDataSchema.describe("If auto creating a customer, the properties from this field will be used.").optional(),
+  entityData: EntityDataSchema.describe("If attaching a product to an entity and auto creating the entity, the properties\nfrom this field will be used. feature_id is required.").optional(),
+  entityId: z.string().nullable().describe("If attaching a product to an entity, can be used to auto create the entity").optional(),
+  forceCheckout: z.boolean().describe("Always return a Stripe Checkout URL, even if the customer's card is already on\nfile").optional(),
+  freeTrial: z.boolean().describe("If the product has a free trial, this field can be used to disable it when\nattaching (by passing in false)").optional(),
   invoice: z.boolean().optional(),
-  options: z.array(CheckoutParamsOptionSchema).nullable().optional(),
-  productId: z.string().nullable().optional(),
-  productIds: z.array(z.string()).nullable().optional(),
-  reward: z.union([z.string(), z.array(z.string())]).optional(),
+  options: z.array(CheckoutParamsOptionSchema).nullable().describe("Pass in quantities for prepaid features").optional(),
+  productId: z.string().nullable().describe("Product ID, set when creating the product in the Autumn dashboard").optional(),
+  productIds: z.array(z.string()).nullable().describe("Can be used to attach multiple products to the customer at once. For example,\nattaching a main product and an add-on.").optional(),
+  reward: z.union([z.string(), z.array(z.string())]).describe("An Autumn promo_code or reward_id to apply at checkout").optional(),
   setupPayment: z.boolean().optional(),
-  skipCheckout: z.boolean().optional(),
-  successUrl: z.string().optional(),
+  successUrl: z.string().describe("URL to redirect to after the purchase is successful").optional(),
   dialog: z.any().optional().describe("Dialog configuration for checkout flow"),
   openInNewTab: z.boolean().optional().describe("Whether to open checkout in a new tab")
 });
@@ -45,38 +44,67 @@ export interface CheckoutParamsOption {
 }
 
 export interface CheckoutParams {
+  /**
+   * Additional parameters to pass onto Stripe when creating the checkout session
+   */
   checkoutSessionParams?: unknown;
 
   /**
-   * Customer data if using attach to auto create customer
+   * If auto creating a customer, the properties from this field will be used.
    */
   customerData?: CustomerData;
 
   /**
-   * Entity data for creating an entity
+   * If attaching a product to an entity and auto creating the entity, the properties
+from this field will be used. feature_id is required.
    */
   entityData?: EntityData;
 
+  /**
+   * If attaching a product to an entity, can be used to auto create the entity
+   */
   entityId?: string | null;
 
+  /**
+   * Always return a Stripe Checkout URL, even if the customer's card is already on
+file
+   */
   forceCheckout?: boolean;
 
+  /**
+   * If the product has a free trial, this field can be used to disable it when
+attaching (by passing in false)
+   */
   freeTrial?: boolean;
 
   invoice?: boolean;
 
+  /**
+   * Pass in quantities for prepaid features
+   */
   options?: CheckoutParamsOption[] | null;
 
+  /**
+   * Product ID, set when creating the product in the Autumn dashboard
+   */
   productId?: string | null;
 
+  /**
+   * Can be used to attach multiple products to the customer at once. For example,
+attaching a main product and an add-on.
+   */
   productIds?: string[] | null;
 
+  /**
+   * An Autumn promo_code or reward_id to apply at checkout
+   */
   reward?: string | Array<string>;
 
   setupPayment?: boolean;
 
-  skipCheckout?: boolean;
-
+  /**
+   * URL to redirect to after the purchase is successful
+   */
   successUrl?: string;
 
   /**
